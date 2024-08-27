87d10355ba7e79ebb07b6ce108d79c86e230aaef
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-08-27 15:38:26 -0300

    Update ML1ex15.py
    Tinha esquecido de fazer esse. Vou arrumar depois ainda já que ficou podre de ruim

--- ML1ex15.py
+++ ML1ex15.py
@@ -1,11 +1,28 @@
-def calcular_media(notas):
-    return sum(notas)/len(notas)
+"""
+15. Faça um programa que leia do teclado as 3 notas bimestrais de um aluno, calcule a
+média e imprima se o aluno está aprovado ou vai para a prova final, sendo a média
+para aprovação igual a 7.
+"""
+
+def definir_aprovacao(notas):
+    media = sum(notas)/len(notas)
+    if media >= 7.0:
+        return "O aluno está aprovado"
+    return "O aluno irá para a prova final"
+
+def verificar_notas(notas):
+    for i in range(len(notas)):
+        if notas[i] < 0.0 or notas[i] > 10.0:
+            raise Exception("Nota(s) devem ser entre zero e dez, fim do programa")
+    return True
 
 def main():
     try:
-        notas = []
-        notas.append(float(input(f"Digite a {i}³ nota: ")) for i in range(0, 3))
-        print(notas[0])
+        notas = [float(input(f"Digite a {i+1}° nota: ")) for i in range(3)]
+        if verificar_notas(notas):
+            print(definir_aprovacao(notas))
+    except ValueError:
+        print("Dado(s) inválido(s), fim do programa")
     except Exception as e:
         print(f"Erro fatal: {e}")
 main()
