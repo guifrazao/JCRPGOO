c6a3c920581dfdf561df47d7b43231759e4042c4
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-08-27 15:22:59 -0300

    Update ML1ex4.py

--- ML1ex4.py
+++ ML1ex4.py
@@ -1,3 +1,8 @@
+"""
+4. Faça um programa que calcule o valor total da venda de uma mercadoria, qualquer
+que seja o número de unidades vendidas e o valor da mercadoria.
+"""
+
 def calcular_venda(qtd_protudo, valor_produto):
     return qtd_protudo * valor_produto
 
