2439a0332699d039df6124cc5adfde1a24171fb1
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-08-28 16:34:19 -0300

    Update ML1ex5.py
    um porre

--- ML1ex5.py
+++ ML1ex5.py
@@ -1,12 +1,17 @@
 """
-5. Faça um programa que calcule a média de um aluno, quaisquer que sejam as suas
-duas notas.
+5. Fazer um programa que calcule e escreva o número de grãos de milho que podem ser
+colocados em um tabuleiro de xadrez, colocando 1 no primeiro quadro e nos quadros
+seguintes o dobro do quadro anterior. Obs.: esse número cresce muito rápido, tenha
+o cuidado de testar se ele não sofre um overflow.
 """
-
-def calcular_media(nota1, nota2):
-    return (nota1 + nota2)/2
+def calcular_milho_tabuleiro(qtd_milho, n_quadros):
+    #tentei deixar isso recursivo mas nao tava querendo funcionar nao
+    for i in range(n_quadros-1, 0, -1):
+        qtd_milho += pow(2, i)
+    return qtd_milho
 
 def main():
-    media = calcular_media(7.6, 8.4)
-    print(media)
+    
+    milho = calcular_milho_tabuleiro(1, 64)
+    print(milho)
 main()
