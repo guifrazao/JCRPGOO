7469161188a88e346e869f2d355986db15596679
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-08-28 16:35:41 -0300

    Update ML1ex15.py

--- ML1ex15.py
+++ ML1ex15.py
@@ -1,28 +1,13 @@
 """
-15. Faça um programa que leia do teclado as 3 notas bimestrais de um aluno, calcule a
-média e imprima se o aluno está aprovado ou vai para a prova final, sendo a média
-para aprovação igual a 7.
+15. Elabore um programa que calcule e mostre a soma dos 10 primeiros termos da série: 100/0! + 99/1! + 98/2! + 97/3!...
 """
-
-def definir_aprovacao(notas):
-    media = sum(notas)/len(notas)
-    if media >= 7.0:
-        return "O aluno está aprovado"
-    return "O aluno irá para a prova final"
-
-def verificar_notas(notas):
-    for i in range(len(notas)):
-        if notas[i] < 0.0 or notas[i] > 10.0:
-            raise Exception("Nota(s) devem ser entre zero e dez, fim do programa")
-    return True
+from math import factorial
+def calcular_serie(n_inicial, qtd_series):
+    if qtd_series == 0:
+        return n_inicial/factorial(100-n_inicial)
+    return n_inicial/factorial(100-n_inicial) + calcular_serie(n_inicial-1, qtd_series-1)
 
 def main():
-    try:
-        notas = [float(input(f"Digite a {i+1}° nota: ")) for i in range(3)]
-        if verificar_notas(notas):
-            print(definir_aprovacao(notas))
-    except ValueError:
-        print("Dado(s) inválido(s), fim do programa")
-    except Exception as e:
-        print(f"Erro fatal: {e}")
+    dez_primeiros = calcular_serie(100, 10)
+    print(f"Soma dos números da série é: {dez_primeiros}")
 main()
