84f8e5f55083ab38196b01af131fd2524f36c476
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-18 15:03:08 -0300

    Update ML1ex5.py

--- ML1ex5.py
+++ ML1ex5.py
@@ -5,7 +5,6 @@ seguintes o dobro do quadro anterior. Obs.: esse número cresce muito rápido, t
 o cuidado de testar se ele não sofre um overflow.
 """
 def calcular_milho_tabuleiro(qtd_milho, n_quadros):
-    #tentei deixar isso recursivo mas nao tava querendo funcionar nao
     for i in range(n_quadros-1, 0, -1):
         qtd_milho += pow(2, i)
     return qtd_milho
