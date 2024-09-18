01a700ba18e507d00bec864eeeca8ed3c96369a9
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-18 19:37:47 -0300

    Update ML1ex5.py

--- ML1ex5.py
+++ ML1ex5.py
@@ -4,13 +4,13 @@ colocados em um tabuleiro de xadrez, colocando 1 no primeiro quadro e nos quadro
 seguintes o dobro do quadro anterior. Obs.: esse número cresce muito rápido, tenha
 o cuidado de testar se ele não sofre um overflow.
 """
-def calcular_milho_tabuleiro(qtd_milho, n_quadros):
-    for i in range(n_quadros-1, 0, -1):
+def calcular_milho_tabuleiro(n_quadros):
+    qtd_milho = 1
+    for i in range (0, n_quadros):
         qtd_milho += pow(2, i)
     return qtd_milho
 
 def main():
-    
-    milho = calcular_milho_tabuleiro(1, 64)
+    milho = calcular_milho_tabuleiro(64)
     print(milho)
 main()
