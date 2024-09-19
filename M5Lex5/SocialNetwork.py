8dd63e8a7428f0921bcb03095db6b01158abda01
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-19 15:30:30 -0300

    Add files via upload

---
+++ M5Lex5/SocialNetwork.py
@@ -0,0 +1,77 @@
+from Post import Post
+from User import User
+class SocialNetwork:
+    __users = []
+    __posts = []
+    def __init__(self):
+        pass
+    
+    """Adds sender as receiver's friend and prints a message to inform that a friend was added"""
+    def addFriend(self, sender, receiver):
+        receiver.addFriend(sender)
+        print(self.__newFriendMsg(sender))
+
+    """Creates Post object, adds it to the posts class attribute and prints the post"""
+    def makePost(self, user, msg):
+        post = Post(user, msg)
+        self.addPost(post)
+        print(f"{post}\n")
+
+    """Creates a dictionary to represent the comment which contains who posted the comment and its content, adds it to the post's comment list and prints the post(?)"""
+    def makeComment(self, post, poster, msg):
+        comm = {"poster":poster.getName(), "msg":msg}
+        post.addComment(comm)
+        print(f"{post}\n")
+
+    def findUser(self, name):
+        user = self.getUser(name)
+        if user is None:
+            print("User not found")
+            exit()
+        return f"{user}"
+        
+    def getUser(self, name):
+        for user in self.__users:
+            if user.getName() == name:
+                return user
+        return None 
+
+    def getPost(self, id):
+        for post in self.__posts:
+            if post.getID() == id:
+                return post
+        return None
+    
+    @classmethod
+    def addPost(cls, post):
+        cls.__posts.append(post)
+
+    @classmethod
+    def addUser(cls, user):
+        cls.__users.append(user)
+   
+    def __newFriendMsg(self, user):
+        return f"New friend {user.getName()} added"
+    
+
+        
+
+    
+
+
+conta_teste = User("IRAN FERREIRA")
+conta2 = User("RONALDO FENOMENO")
+teste = SocialNetwork()
+
+teste.addUser(conta_teste)
+teste.addUser(conta2)
+teste.addFriend(conta2, conta_teste)
+teste.makePost(conta_teste, "TESTE")
+teste.makePost(conta_teste, "TESTE2")
+
+post_teste = teste.getPost(1)
+teste.makeComment(post_teste, conta_teste, "RECEBA")
+teste.makeComment(post_teste, conta_teste, "DEVOLVA")
+
+nome = conta_teste.getName()
+print(teste.findUser(nome))
