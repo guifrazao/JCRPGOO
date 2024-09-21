2342777f48b9c9c3ad2b69eee46b585306717f86
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-21 16:57:35 -0300

    Update SocialNetwork.py

--- M5Lex5/SocialNetwork.py
+++ M5Lex5/SocialNetwork.py
@@ -1,3 +1,8 @@
+"""
+5. Crie uma classe chamada SocialNetwork que represente uma rede social online. Essa
+classe deve ter funcionalidades para adicionar amigos, publicar mensagens,
+comentar em posts e buscar por usuários.
+"""
 from Post import Post
 from User import User
 class SocialNetwork:
