b6d4f54429d676a8255b098d87b68fa74d0924a3
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-21 16:57:52 -0300

    Update M5Lex4.py

--- M5Lex4.py
+++ M5Lex4.py
@@ -1,3 +1,8 @@
+"""
+4. Implemente uma classe chamada CardsGame que represente um jogo de cartas
+simples, como o Uno. Essa classe deve ter métodos para embaralhar as cartas,
+distribuir as cartas aos jogadores e permitir jogadas.
+"""
 import random
 class CardsGame:
     """Chosen cards game is Blackjack"""
