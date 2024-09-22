daab4ec5b8add3fb75b2aa12dfeba47ace84e408
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-22 08:57:57 -0300

    Update M5Lex4.py

--- M5Lex4.py
+++ M5Lex4.py
@@ -12,15 +12,16 @@ class CardsGame:
         #the current deck (changes when cards are played)
         self.__curr_cards = []
 
+    """Shuffles the cards, gives them to the players and plays the turns in a loop until a player wins"""
     def playGame(self):
         players = self.getPlayers()
         cards = self.shuffleCards()
         self.giveCards(players, cards)
         while True:
             for player in players:
-                self.playTurn(player, cards)    
+                self.playTurn(player, cards)  
             
-
+    """The player gets the choice of taking a card or passing. If the player takes a card, adds the value of the card to the player's points and updates the deck"""
     def playTurn(self, player, cards):
         if len(self.getPlayers()) == 1:
             self.__awardVictory(player) 
@@ -29,23 +30,17 @@ class CardsGame:
         if input("Do you wish to take a card from the deck? [y/n]").lower() == "y":
             self.addPoints(player, cards)
             print(f"{player['name']}'s points changed to {player['points']}")
-            self.setCurrCards(cards) 
-
-        if self.checkForWinner(player) == "LOST":
+            
+        if self.__checkForWinner(player) == "LOST":
             self.__eliminatePlayer(player)   
             print()            
-        elif self.checkForWinner(player) == "WON":
+        elif self.__checkForWinner(player) == "WON":
             self.__awardVictory(player)
         else:
+            print("Next round") 
             print()
 
-    def checkForWinner(self, player):
-        if player["points"] == 21:
-            return "WON"
-        elif player["points"] > 21:
-            return "LOST"
-        return "CONTINUE"
-
+    """Gets the game's cards, uses the shuffle() method from the random library for shuffling and returns the shuffled deck"""
     def shuffleCards(self):
         cards_shuffled = self.__getCards()
         random.shuffle(cards_shuffled)
@@ -62,9 +57,11 @@ class CardsGame:
     def addPlayer(self, player):
         self.__players.append({"name":player, "points":0})
 
+    """Line 58 example: cards = [{'A': 1}], cards[0].values() = dict_values([1]), list(cards[0].values()) = [1], list(cards[0].values())[0] = 1"""
     def addPoints(self, player, cards):
         player["points"] += list(cards[0].values())[0]
         cards.remove(cards[0])
+        self.setCurrCards(cards) 
     
     def getPlayers(self):
         return self.__players
@@ -77,6 +74,14 @@ class CardsGame:
     
     def setCurrCards(self, curr_cards):
         self.__curr_cards = curr_cards
+
+    """If the player's points is equal to 21, the player wins. If their points are over 21, they lose. Otherwise, the game continues"""
+    def __checkForWinner(self, player):
+        if player["points"] == 21:
+            return "WON"
+        elif player["points"] > 21:
+            return "LOST"
+        return "CONTINUE"
 
     def __awardVictory(self, player):
         print(f"{player['name']} won, end of game")
