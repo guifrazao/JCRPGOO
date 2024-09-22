4329d004a7047c01219ee2b304eec86de721311f
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-09-21 21:09:13 -0300

    Atualizar o noughtsandcrosses.py

--- ex10/noughtsandcrosses.py
+++ ex10/noughtsandcrosses.py
@@ -1,3 +1,18 @@
+'''
+Escreva um programa completo para jogar o jogo da velha. Para tanto crie uma 
+classe NoughtsAndCrosses (jogo da velha):
+• a classe deve conter como dados privados um array bidimensional 3x3 para 
+representar a grade do jogo
+• crie uma enumeração para representar as possibilidades de ocupação de uma 
+casa na grade (vazia, jogador 1 ou jogador 2)
+• o construtor deve inicializar a grade como vazia
+• forneça um método para exibir a grade
+• permita dois jogadores humanos
+• forneça um método para jogar o jogo; todo movimento deve ocorrer em uma 
+casa vazia; depois de cada movimento, determine se houve uma derrota ou um 
+empate.
+'''
+
 class NoughtsAndCrosses:
     def __init__(self):
         self.player_1 = "X"
