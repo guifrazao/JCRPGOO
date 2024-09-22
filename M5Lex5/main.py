df5d3c94e36bd3bc432fdf305178f241709eb788
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-09-22 09:00:16 -0300

    Update main.py

--- M5Lex5/main.py
+++ M5Lex5/main.py
@@ -1,21 +1,26 @@
+"""
+5. Crie uma classe chamada SocialNetwork que represente uma rede social online. Essa
+classe deve ter funcionalidades para adicionar amigos, publicar mensagens,
+comentar em posts e buscar por usuários.
+"""
 from SocialNetwork import SocialNetwork
 from Post import Post
 from User import User
 
 iran = User("IRAN FERREIRA")
 ronaldo = User("RONALDO FENOMENO")
-teste = SocialNetwork()
+network = SocialNetwork()
 
-teste.addUser(iran)
-teste.addUser(ronaldo)
-teste.addFriend(ronaldo, iran)
-teste.makePost(iran, "RECEBA")
-teste.makePost(ronaldo, "SORRISO RONALDO")
+network.addUser(iran)
+network.addUser(ronaldo)
+network.addFriend(ronaldo, iran)
+network.makePost(iran, "RECEBA")
+network.makePost(ronaldo, "SORRISO RONALDO")
 
-post1 = teste.getPost(1)
-post2 = teste.getPost(2)
-teste.makeComment(post1, ronaldo, "RONALDO")
-teste.makeComment(post2, iran, "MELHOR DO MUNDO")
+post1 = network.getPost(1)
+post2 = network.getPost(2)
+network.makeComment(post1, ronaldo, "RONALDO")
+network.makeComment(post2, iran, "MELHOR DO MUNDO")
 
-nome = iran.getName()
-print(teste.findUser(nome))
+name = iran.getName()
+print(network.findUser(name))
