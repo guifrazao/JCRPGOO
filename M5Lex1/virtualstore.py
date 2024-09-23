3311c9317b71ea6b5dc3f404d38da2e21f88a2af
Gustavo Alves <167457971+SantosGAlves@users.noreply.github.com>
2024-09-22 22:49:46 -0300

    Update virtualstore.py

--- M5Lex1/virtualstore.py
+++ M5Lex1/virtualstore.py
@@ -1,3 +1,9 @@
+'''
+Crie uma classe chamada VirtualStore que represente uma plataforma de vendas
+online. Essa classe deve ter funcionalidades para cadastrar produtos, gerar carrinho
+de compras, aplicar descontos e calcular o valor total da compra.
+'''
+
 class VirtualStore:
     total_products = 0  # Atributo de classe para contar produtos
 
