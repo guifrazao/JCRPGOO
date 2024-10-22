04c42b2a0ba5fe379a20f514eb2f951fe098ef9b
guifrazao <143296175+guifrazao@users.noreply.github.com>
2024-10-22 14:51:09 -0300

    Delete M7L-M3Lex03 directory

--- M7L-M3Lex03/new/account_verifications.py
+++
@@ -1,9 +0,0 @@
-# Conta corrente (permite saldo negativo até -500)
-class CheckingAccountVerification:
-    def verify(self, bank_balance, amount):
-        return (bank_balance - amount) >= -500
-
-# Conta poupança (não permite saldo negativo)
-class SavingsAccountVerification:
-    def verify(self, bank_balance, amount):
-        return amount <= bank_balance
--- M7L-M3Lex03/old/main.py
+++
@@ -1,8 +0,0 @@
-from account import Account
-
-account = Account(500)
-print(account.get_bank_balance()) 
-print(account.debit(501))
-print(account.get_bank_balance()) 
-
-print(help(Account))
