74c6e206ac21ea35db753b7971d4bd3aae093ff3
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2024-10-24 20:20:08 -0300

    Delete M7L-M3Lex01/new/M5Lex07 directory

--- M7L-M3Lex01/new/M5Lex07/New/HolidayProvider.py
+++
@@ -1,7 +0,0 @@
-class HolidayProvider:
-    def __init__(self, carregar_feriados_func):
-        self.carregar_feriados_func = carregar_feriados_func
-
-    def carregar_feriados(self, ano):
-        return self.carregar_feriados_func(ano)
-
