cafaa991495865d2c5251f05e9862c9c049f1d21
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:16:01 -0300

    Update guessinggame.py

--- M5Lex08/New/Modulos/guessinggame.py
+++ M5Lex08/New/Modulos/guessinggame.py
@@ -1,3 +1,10 @@
+'''
+M5L-ex08. Implemente uma classe chamada GuessingGame que represente um jogo de 
+adivinhação. Essa classe deve gerar um número aleatório, permitir que o jogador faça 
+palpites e informar se o palpite está correto, informando se é maior ou menor que o 
+número gerado. 
+'''
+
 import random
 from .player import Player
 
