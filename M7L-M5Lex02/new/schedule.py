d2d1872a858ab50b445ab881502ac8d3dc2a1e97
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:07:50 -0300

    Update schedule.py

--- M7L-M5Lex02/new/schedule.py
+++ M7L-M5Lex02/new/schedule.py
@@ -1,3 +1,9 @@
+'''
+M5L-ex02. Implemente uma classe chamada Schedule que represente uma agenda telefônica. 
+Essa classe deve permitir adicionar, editar e remover contatos, além de buscar por 
+contatos a partir de um nome ou número de telefone.
+'''
+
 class Schedule:
     def __init__(self):
         self.phone_book = []
