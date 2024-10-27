8d8046dd4db260dcc89dd821705f70cb80085648
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:09:21 -0300

    Update SocialNetwork.py

--- M7Lex05/new/SocialNetwork.py
+++ M7Lex05/new/SocialNetwork.py
@@ -1,5 +1,5 @@
 """
-5. Crie uma classe chamada SocialNetwork que represente uma rede social online. Essa
+M5L-ex05. Crie uma classe chamada SocialNetwork que represente uma rede social online. Essa
 classe deve ter funcionalidades para adicionar amigos, publicar mensagens,
 comentar em posts e buscar por usuários.
 """
