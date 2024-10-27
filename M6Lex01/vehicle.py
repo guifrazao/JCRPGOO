cb42067e087e029ea2ff8029be8d658f5b97904b
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 15:56:27 -0300

    Update vehicle.py

--- M6Lex01/vehicle.py
+++ M6Lex01/vehicle.py
@@ -1,3 +1,9 @@
+'''
+Escreva uma classe chamada Vehicle que possua campos para a velocidade atual em 
+km/h, a direção em graus dos pneus e o nome do proprietário. Crie métodos de 
+acesso e impressão para esta classe e faça um programa de teste. 
+'''
+
 from owner import Owner
 class Vehicle:
     def __init__(self, current_speed, direction, owner: Owner):
