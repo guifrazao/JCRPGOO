66698965253fda7676a06c45210242c50b0563ec
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:00:59 -0300

    Update employee.py

--- M6Lex08/employee.py
+++ M6Lex08/employee.py
@@ -1,3 +1,13 @@
+'''
+M3L-ex08. Crie uma classe chamada Employee que inclui três variáveis de instância: um nome 
+(string), um sobrenome (string) e um salário mensal (float). Sua classe deve ter um 
+construtor que inicializa as três variáveis. Forneça um método get e set para cada 
+variável. Se o salário mensal fornecido pelo usuário não for positivo, configure-o 
+como 0.0. Teste a classe implementada e seus métodos. Crie dois objetos Employee e 
+exiba o salário anual de cada objeto. Depois, dê 10% de aumento para cada 
+empregado e exiba novamente os salários. 
+'''
+
 class Employee:
     def __init__(self, name, surname, monthly_salary):
         if not self.__verifyName(name):
