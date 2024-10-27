c8b7ebec1dfa6255d981eaf5175b2993bc7920c9
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:21:55 -0300

    Update supplier.py

--- M10Lex01-03/supplier.py
+++ M10Lex01-03/supplier.py
@@ -1,3 +1,15 @@
+'''
+M10L-ex02. Considere, como subclasse da classe Person, a classe Supplier, para representar um 
+fornecedor. Considere que cada instância da classe Supplier tem, para além dos
+atributos que caracterizam a classe Person, os atributos valueCredit (correspondente 
+ao crédito máximo atribuído ao fornecedor) e valueDebt (montante da dívida para com 
+o fornecedor). Implemente na classe Supplier, para além dos usuais métodos 
+seletores e modificadores, um método getBalance() que devolve a diferença entre os 
+valores dos atributos valueCredit e valueDebt. Depois de implementada a classe 
+Supplier, altere o main para que você possa verificar o funcionamento dos métodos 
+implementados na classe Supplier e os herdados da classe Person. 
+'''
+
 from pessoa import Pessoa
 
 class Supplier(Pessoa):
