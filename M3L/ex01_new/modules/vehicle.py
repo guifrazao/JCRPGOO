61dd5800d6ffb1a749ebdd361ced89eccf34c8da
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:14:16 -0300

    Update vehicle.py

--- M3L/ex01_new/modules/vehicle.py
+++ M3L/ex01_new/modules/vehicle.py
@@ -1,3 +1,9 @@
+'''
+M3L-ex01. Escreva uma classe chamada Vehicle que possua campos para a velocidade atual em 
+km/h, a direção em graus dos pneus e o nome do proprietário. Crie métodos de 
+acesso e impressão para esta classe e faça um programa de teste. 
+'''
+
 class Vehicle:
     def __init__(self, current_speed, direction):
         self.__current_speed = current_speed
