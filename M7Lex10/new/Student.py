5e2d50ea64f68b39205bc2bed150ae8b30e341e2
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:07:09 -0300

    Update Student.py

--- M7Lex10/new/Student.py
+++ M7Lex10/new/Student.py
@@ -1,3 +1,13 @@
+'''
+M3L-ex10. Adicione três métodos à classe Student estudada em sala que compara dois objetos 
+Student. Um método deve testar a igualdade. Um segundo método deve testar para 
+menor que. O terceiro método deve testar para maior que ou igual a. Em cada caso, o 
+método retorna o resultado da comparação dos nomes dos dois alunos. Inclua uma 
+função main que testa todos os operadores de comparação. Em seguida, coloque 
+vários objetos Student em uma lista e embaralhe. Em seguida, execute o método sort 
+com esta lista e exiba todas as informações dos alunos. 
+'''
+
 '''File: studentATT.py
 Features for managing a student's name and grades'''
 class Student:
