6caea9be30ffe0e7bb83dcab45c5e8655449cb8f
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:14:49 -0300

    Update account.py

--- M3L/ex03_new/modules/account.py
+++ M3L/ex03_new/modules/account.py
@@ -1,3 +1,11 @@
+'''
+M3L-ex03. Crie uma classe Account que representa uma conta bancária. A classe deve fornecer 
+um método chamado debit para retirar dinheiro da conta. Assegure que a quantia de 
+débito não exceda o saldo de Account. Se exceder, o saldo deve ser deixado 
+inalterado e o método deve imprimir uma mensagem que indica “Quantia de débito 
+excedeu o saldo da conta”. Teste a classe implementada e seus métodos. 
+'''
+
 class Account:
     def __init__(self, bank_balance):
         self.__bank_balance = bank_balance  # bank_balance = saldo bancário
