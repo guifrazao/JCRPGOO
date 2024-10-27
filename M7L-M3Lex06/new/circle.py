7ddff8ee73d98f1795bbb2d122286b8c838908d5
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:05:01 -0300

    Update circle.py

--- M7L-M3Lex06/new/circle.py
+++ M7L-M3Lex06/new/circle.py
@@ -1,3 +1,14 @@
+'''
+M3L-ex06. Escreva uma classe Circle composta de um ponto para indicar o centro e o tamanho 
+do raio, que não deve ser menor que zero. Utilize a classe Point criada anteriormente. 
+Crie métodos get e set para testar sua classe. A seguir crie:  
+a. Um método que retorne a área do círculo. 
+b. Um método move que movimente o centro do círculo (utilize o método move 
+da classe ponto). 
+Crie e teste um construtor para a classe Circle com todos os valores zerados, mas 
+que também possa receber valores dados.
+'''
+
 from point import Point
 import math
 
