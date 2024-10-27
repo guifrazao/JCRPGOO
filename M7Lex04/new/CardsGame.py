fd25627951bea0e378d185b60131d7b8e0618208
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-10-27 16:08:51 -0300

    Update CardsGame.py

--- M7Lex04/new/CardsGame.py
+++ M7Lex04/new/CardsGame.py
@@ -1,5 +1,5 @@
 """
-4. Implemente uma classe chamada CardsGame que represente um jogo de cartas
+M5L-ex04. Implemente uma classe chamada CardsGame que represente um jogo de cartas
 simples, como o Uno. Essa classe deve ter métodos para embaralhar as cartas,
 distribuir as cartas aos jogadores e permitir jogadas.
 """
