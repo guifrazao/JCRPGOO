0e58850d6b631d53ad966d7dcf178f913313b4a0
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2024-10-28 07:06:22 -0300

    Update main.py

--- M3L/M6Lex02/New/main.py
+++ M3L/M6Lex02/New/main.py
@@ -1,3 +1,18 @@
+"""
+Escolha pelo menos 5 exercícios das listas M3L e M5L (5 de cada) para expandir o projeto do
+exercício, incluindo novas classes relacionadas, conforme a sua criatividade, demonstrando
+a injeção de dependência.
+
+Escreva uma classe chamada Bicycle que possua campos para a velocidade,
+cadência dos pedais (número de rotações dos pedais por minuto), marcha atual e
+número de série. A velocidade e a cadência dos pedais não podem ser menores que
+zero, a marcha atual deve estar entre 1 e 18 e o número de série deve ser maior que
+1000. Crie constantes simbólicas e métodos de acesso e impressão que reflitam
+esses limites. Teste a classe implementada e seus métodos. A seguir, crie um método
+que calcule a velocidade relativa entre a bicicleta e outra dada como parâmetro. Teste
+o seu novo método.
+"""
+
 from owner import Owner
 from bicycle import Bicycle
 from validator import Validator_Bicycle, Validator_Owner, Validator_Roda
