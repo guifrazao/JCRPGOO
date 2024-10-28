200a4d95014f16725cc31a96e7065e920925f0d9
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2024-10-28 07:09:11 -0300

    Update main.py

--- M5Lex09/New/main.py
+++ M5Lex09/New/main.py
@@ -1,3 +1,25 @@
+"""
+Escolha pelo menos 5 exercícios das listas M3L e M5L (5 de cada) para expandir o projeto do
+exercício, incluindo novas classes relacionadas, conforme a sua criatividade, demonstrando
+a associação bilateral.
+
+Crie uma classe chamada Invoice que possa ser utilizado por uma loja de
+suprimentos de informática para representar uma fatura de um item vendido na loja.
+Uma fatura deve incluir as seguintes informações como atributos:
+• o número do item faturado,
+• a descrição do item,
+• a quantidade comprada do item e
+• o preço unitário do item.
+Sua classe deve ter um construtor que inicialize os quatro atributos. Se a quantidade
+não for positiva, ela deve ser configurada como 0. Se o preço por item não for
+positivo ele deve ser configurado como 0.0. Forneça um método set e um método get
+para cada variável de instância. Além disso, forneça um método chamado que calcula
+o valor da fatura (isso é, multiplica a quantidade pelo preço por item) e depois retorna
+o valor real. Escreva um aplicativo de teste que demonstra as capacidades da classe
+Invoice.
+""" 
+
+
 from Modulos import *  # Importa todas as classes e funções do módulo Modulos
 
 
