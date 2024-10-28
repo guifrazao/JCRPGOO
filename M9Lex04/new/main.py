635c9eceee4195bf1828b82cfb2434f9dca59c83
Gustavo Alves <167457971+SantosGAlves@users.noreply.github.com>
2024-10-28 08:28:06 -0300

    Update main.py

--- M9Lex04/new/main.py
+++ M9Lex04/new/main.py
@@ -1,12 +1,12 @@
 from CardsGame import CardsGame
 from Blackjack import Blackjack
-from Uno import Uno
 
 blackjack = Blackjack()
-game = CardsGame()
+game = CardsGame(blackjack)
+blackjack.setCardsGame(game)
+
 game.addPlayer("IRAN")
 game.addPlayer("RONALDO")
 game.addPlayer("GARRINCHA")
 
-game.playGame(Uno())
-game.playGame(Blackjack())
+game.playGame()
