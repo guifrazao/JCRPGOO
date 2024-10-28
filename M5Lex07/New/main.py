7b286deab1f22cf2b36fa9a0a8058f0635a9b7f3
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2024-10-28 06:57:51 -0300

    Update main.py

--- M5Lex07/New/main.py
+++ M5Lex07/New/main.py
@@ -1,4 +1,7 @@
 """
+Refazer as listas M3L e M5L, aplicando o Princípio Aberto-Fechado e mostrar as diferenças
+de seu código, antes e depois.
+
 Crie uma classe chamada Calendar que represente um calendário anual. Essa classe
 deve ter métodos para exibir o calendário de um determinado mês, verificar se uma
 data é feriado e calcular a diferença de dias entre duas datas.
