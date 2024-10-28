0ad62fa08411f166e932bacfbce7f60929104917
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2024-10-28 06:55:33 -0300

    Update main.py

--- M4Lex02/Old/main.py
+++ M4Lex02/Old/main.py
@@ -1,3 +1,14 @@
+"""
+Escreva uma classe chamada Bicycle que possua campos para a velocidade,
+cadência dos pedais (número de rotações dos pedais por minuto), marcha atual e
+número de série. A velocidade e a cadência dos pedais não podem ser menores que
+zero, a marcha atual deve estar entre 1 e 18 e o número de série deve ser maior que
+1000. Crie constantes simbólicas e métodos de acesso e impressão que reflitam
+esses limites. Teste a classe implementada e seus métodos. A seguir, crie um método
+que calcule a velocidade relativa entre a bicicleta e outra dada como parâmetro. Teste
+o seu novo método.
+"""
+
 from bicycle import Bicycle
 from validatorVel import VelocidadeValidator
 from validatorCad import CadenciaValidator
