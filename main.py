4b61ff88fe2cde2d6718d2262249196938e2613e
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-12-01 20:52:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from classes import *
 
-product1 = Product(1, 10.0, "Caderno")
-product2 = Product(2, 5.0, "Caneta")
-
-item1 = ItemOrder(product1, 3)  
-item2 = ItemOrder(product2, 2)  
+product1 = Product(1, 50.0, "Camiseta")
+product2 = Product(2, 120.0, "Calça Jeans")
+product3 = Product(3, 30.0, "Tênis")
 
 order = Order()
-order.addItem(item1)
-order.addItem(item2)
 
-print(f"Total do pedido: R$ {order.getTotal():.2f}")
+order.addItem(product1, 2)  
+order.addItem(product2, 1)  
+order.addItem(product3, 3)
+
+order.finalizePurchase()
